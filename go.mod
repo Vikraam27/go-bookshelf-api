8e437ee1f3d8cecfa6f25d634e2995b47cf3b9d9
vikraam27 <ramvik36@gmail.com>
2022-10-05 11:01:01 +0700

    adding POST book and GET all book

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Vikraam27/go-bookshelf-api
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 type Response struct {
 	Status  string `json:"status"`
@@ -8,23 +18,106 @@ type Response struct {
 	Data    *Data  `json:"data"`
 }
 
-type Data struct {
-	Book []Book `json: "books"`
+type ResponseWithOutData struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+type ResponseAddBook struct {
+	Status  string  `json:"status"`
+	Message string  `json:"message"`
+	BookId  *BookId `json:"data"`
+}
+
+type BookId struct {
+	BookId string `json:"bookId"`
 }
 
 type Book struct {
+	Id         string `json:"id"`
+	Name       string `json:"name"`
+	Year       uint   `json:"year"`
+	Author     string `json:"author"`
+	Summary    string `json:"summary"`
+	Publisher  string `json:"publisher"`
+	PageCount  uint   `json:"pageCount,"`
+	ReadPage   uint   `json:"readPage,"`
+	Finished   bool   `json:"finished,"`
+	Reading    bool   `json:"reading,"`
+	InsertedAt string `json:"insertedAt,"`
+	UpdatedAt  string `json:"updatedAt,"`
+}
+
+type MappedBook struct {
 	Id        string `json:"id"`
-	Title     string `json:"title"`
+	Name      string `json:"name"`
 	Publisher string `json:"publisher"`
+}
+type Data struct {
+	Book []MappedBook `json:"books"`
+}
+
+type ClientError struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
 var response Response
 var books []Book
 
-func main() {
-	books = append(books, Book{Id: "1", Title: "book1", Publisher: "name"})
-	response = Response{Status: "success", Message: " success", Data: &Data{Book{books}}}
+func getAllBooks(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-type", "application/json; charset=utf-8")
+	var mappedBook []MappedBook
+	for _, item := range books {
+		mappedBook = append(mappedBook, MappedBook{Id: item.Id, Name: item.Name, Publisher: item.Publisher})
+	}
+	response = Response{Status: "success", Message: " successfully get all books", Data: &Data{Book: mappedBook}}
+	json.NewEncoder(res).Encode(response)
+}
 
-	fmt.Println(response)
+func addBook(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-type", "application/json; charset=utf-8")
+	var book Book
+	_ = json.NewDecoder(req.Body).Decode(&book)
+
+	book.Id = strconv.Itoa(rand.Intn(1000000))
+	book.InsertedAt = time.Now().Local().String()
+	book.UpdatedAt = time.Now().Local().String()
+
+	if book.ReadPage == book.PageCount {
+		book.Finished = true
+	} else {
+		book.Finished = false
+	}
+
+	if book.Name == "" {
+		res.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(res).Encode(ClientError{Status: "fail", Message: "Gagal menambahkan buku. Mohon isi nama buku"})
+
+	} else if book.ReadPage > book.PageCount {
+		res.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(res).Encode(ClientError{Status: "fail", Message: "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount"})
+	} else if !(book.Name == "" && book.ReadPage > book.PageCount) {
+		books = append(books, book)
+		res.WriteHeader(http.StatusCreated)
+		for _, item := range books {
+			if item.Id == book.Id {
+				json.NewEncoder(res).Encode(ResponseAddBook{Status: "success", Message: "Buku berhasil ditambahkan", BookId: &BookId{book.Id}})
+			}
+		}
+	} else {
+		res.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(res).Encode(ClientError{Status: "fail", Message: "gagal menambahkan buku"})
+	}
+}
+
+func main() {
+	routes := mux.NewRouter()
+
+	routes.HandleFunc("/books", getAllBooks).Methods("GET")
+	routes.HandleFunc("/books", addBook).Methods("POST")
+	fmt.Printf("Starting server at port 8000\n")
+
+	log.Fatal(http.ListenAndServe(":8000", routes))
 
 }
